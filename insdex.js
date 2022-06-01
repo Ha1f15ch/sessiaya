01cebcd419394aeead726875c3b43b73ab376d27
Alexey <alexy.alexy98@mail.ru>
2022-06-02 00:27:58 +0300

    Первое задание

--- insdex.js
+++ insdex.js
@@ -1,4 +1,20 @@
-console.log('aaaaaa');
-() => {
-    console.log('AAAAA<<<<M<MMMC<')
-}
+const { query } = require('express');
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+
+    var days = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'];
+
+      var d = new Date();
+      var n = d.getDay();
+      
+    //console.log(req.query)
+    // http://localhost:3000/?name=John
+    res.send(`Hello ${req.query.name}, today is ${days[n]}`);
+})
+
+app.listen(port, () => {
+    console.log(`Сервер работает на порте ${port}`)
+})

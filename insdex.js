077960d9769c03113a8c6e048b4c152b01f56967
Alexey <alexy.alexy98@mail.ru>
2022-06-03 02:16:46 +0300

    Zadanie 4

--- insdex.js
+++ insdex.js
@@ -6,17 +6,7 @@ const port = 3000
 const urlencodedParser = express.urlencoded({extended: false})//Значение extended: false указывает, 
 //что объект - результат парсинга будет представлять набор пар ключ-значение, а каждое значение может быть представлено в виде строки или массива.
 app.get('/', (req, res) => {
-  res.redirect('/list')
-})
-
-app.get('/list', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-})
-
-app.post('/Create', urlencodedParser, (req, res) => {
-  if(!req.body) {return res.sendStatus(400);}
-  console.log(req.body);
-  res.send(`${req.body.DataUser}`);
+  res.send('Title LIST')
 })
 
 app.listen(port, () => {
